976e59abe012a4bfdbaca035f8a014195b10b759
William Yaworsky <will.yaworsky@gmail.com>
2015-04-11 13:50:56 -0400

    connected to spark core

--- energy_gauntlet/spark.py
+++ energy_gauntlet/spark.py
@@ -4,16 +4,18 @@ from spyrk import SparkCloud
 from commands import *
 import threading
 
-spark_client = None # SparkCloud(os.getenv('ACCESS_TOKEN'))
+spark_connection = SparkCloud(os.getenv('ACCESS_TOKEN'))
 
 class Spark:
 
-  def __init__(self, spark_client = None):
-    self.client = spark_client
+  def __init__(self, spark_connection):
+    self.device = spark_connection.devices['carls']
 
   def get_raw(self):
-    return {
-      'leftFinger': 50 #self.client.analogread('A0')
+    return { 'connected': False } if not self.device.connected else {
+      'connected': True,
+      'flex0':     self.device.flex0,
+      'button0':   self.device.button0
     }
 
   # rake raw and do something to figure out what commands to do
@@ -28,4 +30,4 @@ class Spark:
       ][(int(time.time()) % 8) / 2]
     ]
 
-spark = Spark(spark_client)
+spark = Spark(spark_connection)
