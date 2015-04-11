6c87e9f496aaf8173d9c9b89290df400a2e956d6
William Yaworsky <will.yaworsky@gmail.com>
2015-04-11 12:39:59 -0400

    bluemix working

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ dotenv.load_dotenv(os.path.dirname(os.path.realpath(__file__)) + "/.env")
 from energy_gauntlet.server import application
 
 if __name__ == "__main__":
-  application.listen(8888)
+  application.listen(os.getenv('PORT', 8888))
   tornado.ioloop.IOLoop.instance().start()
