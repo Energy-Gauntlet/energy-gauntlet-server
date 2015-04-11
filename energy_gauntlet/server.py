0a9468b4d616d2741a62bd8e1aa2da25563c963e
William Yaworsky <will.yaworsky@gmail.com>
2015-04-11 15:55:10 -0400

    better polling for spark

--- energy_gauntlet/server.py
+++ energy_gauntlet/server.py
@@ -1,5 +1,6 @@
 import os
 import tornado.web
+import tornado.websocket
 from tornado.escape import json_encode
 from spark import spark
 import threading
@@ -27,9 +28,36 @@ class RawHandler(tornado.web.RequestHandler):
     self.set_header('Content-Type', 'application/json')
     self.write(json_encode(spark.get_raw()))
 
+class SocketHandler(tornado.websocket.WebSocketHandler):
+  clients = set()
+
+  def open(self):
+    ChatSocketHandler.waiters.add(self)
+
+  def on_close(self):
+    ChatSocketHandler.waiters.remove(self)
+
+class RawSocketHandler(SocketHandler):
+  def send_raw(self, data):
+    for waiter in cls.waiters:
+      try:
+          waiter.write_message(data)
+      except:
+        pass
+
+class CommandsSocketHandler(SocketHandler):
+  def send_commands(self, data):
+    for waiter in cls.waiters:
+      try:
+          waiter.write_message(data)
+      except:
+        pass
+
 application = tornado.web.Application([
   (r"/", IndexHandler),
   (r"/raw", RawHandler),
   (r"/what-should-i-do", CommandHandler),
   (r'/(.*)', tornado.web.StaticFileHandler, {'path': config.static_path }),
+  (r'/ws/raw', RawSocketHandler),
+  (r'/ws/commands', CommandsSocketHandler),
 ])
--- energy_gauntlet/spark.py
+++ energy_gauntlet/spark.py
@@ -7,8 +7,21 @@ import threading
 class Spark:
 
   def __init__(self, token):
-    self.token = token
-    self.connect()
+    self.token  = token
+    self.device = None
+    # self.connect()
+    self._update_loop()
+
+  def _update_loop(self):
+    d = {}
+    if self.device and self.device.connected:
+      for k in self.device.variables.keys():
+        d[k] = getattr(self.device, k)
+      self.raw = d
+    else:
+      self.connect()
+      self.raw = { 'connected': False }
+    threading.Timer(0.5, self._update_loop).start()
 
   def connect(self):
     try:
@@ -18,16 +31,15 @@ class Spark:
       pass
 
   def get_raw(self):
-    if not self.device.connected:
-      self.connect()
-      return {
-        'connected': False
-      }
-    else:
-      d = {}
-      for k in self.device.variables.keys():
-        d[k] = getattr(self.device, k)
-      return d
+    # d = {}
+    # if self.device and self.device.connected:
+    #   for k in self.device.variables.keys():
+    #     d[k] = getattr(self.device, k)
+    #   self.raw = d
+    # else:
+    #   self.connect()
+    #   self.raw = { 'connected': False }
+    return self.raw
 
   # rake raw and do something to figure out what commands to do
   def get_commands(self):
