a6504036d60e535d43d2e29511a38c423a2a1c62
William Yaworsky <will.yaworsky@gmail.com>
2015-04-11 21:48:31 -0400

    trying different method to set port

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ dotenv.load_dotenv(os.path.dirname(os.path.realpath(__file__)) + "/.env")
 from energy_gauntlet.app import application
 
 if __name__ == "__main__":
-  application.listen(os.getenv('PORT', 8888))
+  application.listen(os.environ.get('PORT', 8888))
   tornado.ioloop.IOLoop.instance().start()
