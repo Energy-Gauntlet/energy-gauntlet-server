2c3de1b0cf44f884c724b90bfe5c7df86df711ad
William Yaworsky <will.yaworsky@gmail.com>
2015-04-11 22:37:37 -0400

    added latency measure

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,16 @@
 var makeWsListener = function(path, selector) {
   var ws = new WebSocket('ws://' + location.host + path);
 
+  var last = 0;
+  var diff = 0;
+
   ws.onmessage = function(event) {
+    var now = window.performance.now();
+    diff    = now - last;
+    last    = now;
     var str = JSON.stringify(JSON.parse(event.data), undefined, 2);
     $(selector).text(str);
+    $(selector).parent().parent().find('.latency').text(diff.toFixed(2));
   };
 
   ws.onerror = function(event) {
