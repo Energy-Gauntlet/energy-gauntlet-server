69a1c3e5350827d847acac20858265634659a476
William Yaworsky <will.yaworsky@gmail.com>
2015-04-17 10:48:51 -0400

    added sayings for krt demo

--- static/js/say.js
+++ static/js/say.js
@@ -1,13 +1,18 @@
 [
 
-  '',
   'Hello',
   'Come at me bro!',
   'Rocky, I love you',
   'I am Gilligan',
   'The last boat I was on got shipwrecked',
   'Gee whiz, Professor!',
-  'I am the droid you are looking for'
+  'I am the droid you are looking for',
+
+  'Kirkland robotics team',
+  'Nice to meet you',
+
+  'Energy Gauntlet',
+  'I am controlled with a glove'
 
 ].forEach(function(text) {
   $('#say-body').append('<form action="/say" method="post"> <input type="text" name="say" value="' + text + '"> <button type="submit">Submit</button></form>');
