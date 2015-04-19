7f95a26413e69d60df80eb4517c2e2bcbb7f581a
William Yaworsky <will.yaworsky@gmail.com>
2015-04-19 13:31:23 -0400

    more sayings

--- static/js/say.js
+++ static/js/say.js
@@ -1,10 +1,15 @@
 [
 
   'Hello',
+
+  'My name is gil a gan',
+  'I am controled using the energy gauntlet',
+
   'Come at me bro!',
   'Rocky, I love you',
-  'I am Gilligan',
-  'The last boat I was on got shipwrecked',
+  'I am gil a gan',
+  'The last boat I was on, got shipwrecked',
+  'We were stranded for what seemed like three seasons',
   'Gee whiz, Professor!',
   'I am the droid you are looking for',
 
@@ -12,7 +17,7 @@
   'Nice to meet you',
 
   'Energy Gauntlet',
-  'I am controlled with a glove'
+  'I am controled with a glove'
 
 ].forEach(function(text) {
   $('#say-body').append('<form action="/say" method="post"> <input type="text" name="say" value="' + text + '"> <button type="submit">Submit</button></form>');
